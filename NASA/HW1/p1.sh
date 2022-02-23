f14d4d7126919a294aaceca2779eaae4f24b5fb0
701-Coder <51012111+701-coder@users.noreply.github.com>
2022-02-23 16:25:29 +0800

    Add files via upload

--- NASA/HW1/p1.sh
+++ NASA/HW1/p1.sh
@@ -1,59 +1,47 @@
-function WA(){
-    echo 'usage: ./p1.sh -s <Unix timestamp > -e <Unix timestamp > <input file>' >&2
-    echo 'ex: ./p1.sh -s 1133642864 -e 1133643505 log1.txt log3.txt' >&2
+#!/bin/bash
+
+WA(){
+    echo 'usage: ./p1.sh -s <Unix timestamp > -e <Unix timestamp > <input file>'
+    echo 'ex: ./p1.sh -s 1133642864 -e 1133643505 log1.txt log3.txt'
     exit 1
 }
 
-declare -A args
-i=0
-for arg in $@; do
-    args[$i]=$arg
-    ((i++))
-done
-s=""
-e=""
-is=0
-ie=0
-rem='\-'
-for ((i=0; i < $#; i++)); do
-    if [[ ${args[$i]} == '-s' ]]; then
-        if [[ -z ${args[$(($i+1))]} ]]; then
-            WA
-        fi
-        s=${args[$(($i+1))]}
-        is=$i
-    elif [[ ${args[$i]} == '-e' ]]; then
-        if [[ -z ${args[$(($i+1))]} ]]; then
-            WA
-        fi
-        e=${args[$(($i+1))]}
-        ie=$i
-    elif [[ ${args[$i]} =~ $rem ]]; then
-        WA
+isNumber(){
+    local __res=$1
+    local re='^[0-9]+$'
+    local ret=0
+    if [[ $2 =~ $re ]]; then
+        ret=1
     fi
+    eval $__res="'$ret'"
+}
+
+while getopts ":s:e:?" argc; do
+    case $argc in
+        s)
+            s=$OPTARG
+            isNumber res $s
+            if [[ $res == 0 ]]; then
+                WA
+            fi
+            ;;
+        e)
+            e=$OPTARG
+            isNumber res $e
+            if [[ $res == 0 ]]; then
+                WA
+            fi
+            ;;
+        ?)
+            WA
+            ;;
+    esac
 done
 
-if [[ $s == "" ]] || [[ $e == "" ]]; then
+if ! [[ $s ]] || ! [[ $e ]] || [[ $s -gt $e ]]; then
     WA
 fi
 
-re='^[0-9]+$';
-
-if ! [[ $s =~ $re ]] || ! [[ $e =~ $re ]]; then
-    WA
-fi
-
-if [[ $s -gt $e ]]; then
-    WA
-fi
-
-f=-1
-
-for ((i=0; i < $#; i++)); do
-    if ! [[ ${args[$i]} =~ $rem ]] && [[ $i != $(($is+1)) ]] && [[ $i != $(($ie+1)) ]]; then
-        f=$i
-    fi
-done
-if [[ $f == -1 ]]; then
+if [[ -z $5 ]]; then
     WA
 fi
