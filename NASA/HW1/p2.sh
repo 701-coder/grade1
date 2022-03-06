7634cc7c53718ebb070177a468f2af1315cabf27
701-Coder <51012111+701-coder@users.noreply.github.com>
2022-03-06 09:32:54 +0800

    Add files via upload

--- NASA/HW1/p2.sh
+++ NASA/HW1/p2.sh
@@ -4,7 +4,11 @@ c='ls'
 I=''
 que0=''
 que1=''
+Que1=''
 NEWLINE='@'
+sym=0
+sl=0
+slFile=''
 
 dfs(){
     s=''
@@ -14,6 +18,29 @@ dfs(){
     if [[ $1 -gt 0 ]]; then
         s="|$s "
     fi
+    S=`pwd -P`
+    Que1="$Que1$NEWLINE$S"
+    lp=0
+
+    if [[ $3 != '.' ]] && [[ -d $3 ]]; then
+        cd $3
+        S=`pwd -P`
+        ifs0=$IFS
+        IFS="$NEWLINE"
+        read -ra A <<< "$Que1"
+        for i in "${A[@]}"; do
+            if [[ $i == $S ]]; then
+                lp=1
+            fi
+        done
+        IFS=$ifs0
+        cd ..
+    fi
+
+    if [[ $sl == 1 ]]; then
+        lp=0
+    fi
+
     if [[ $I == '' ]] || [[ $3 =~ $I ]]; then
         ifs0=$IFS
         IFS="$NEWLINE"
@@ -26,38 +53,71 @@ dfs(){
                 que2=1
             fi
         done
-        echo "$s$3"
-        #echo "$que1$s$3"
+        if [[ $lp == 1 ]]; then
+            echo "$s$3 (loop)"
+        elif [[ $sl == 1 ]]; then
+            echo "$s$3 -> $slFile"
+        else
+            echo "$s$3"
+        fi
         que0="$que1$s$3"
         IFS=$ifs0
     fi
 
     que1="$que1$s$3$NEWLINE"
 
-    if [[ -d $3 ]]; then
+    if [[ -d $3 ]] && [[ $lp == 0 ]]; then
         cd $3
         if [[ $1 -le $2 ]]; then
-            for i in `$c`; do
-                if [[ $i != '.' ]] && [[ $i != '..' ]]; then
-                    dfs $(($1+1)) $2 $i
-                fi
-            done
+            if [[ $sym == 0 ]]; then
+                for i in `$c`; do
+                    if [[ $i != '.' ]] && [[ $i != '..' ]]; then
+                        dfs $(($1+1)) $2 $i
+                    fi
+                done
+            elif [[ $sl == 0 ]]; then
+                Line=0
+                while IFS= read -r line; do
+                    if [[ $Line != 0 ]]; then
+                        ifs0=$IFS
+                        read -ra A <<< "$line"
+                        IFS=$ifs0
+                        if [[ ${#A[@]} != 9 ]]; then
+                            sl=1
+                            slFile=${A[10]}
+                        else
+                            sl=0
+                            slFile=''
+                        fi
+                        if [[ ${A[8]} != '.' ]] && [[ ${A[8]} != '..' ]]; then
+                            dfs $(($1+1)) $2 ${A[8]}
+                        fi
+                    fi
+                    Line=1
+                done <<< `$c`
+                sl=0
+            fi
         fi
         cd ..
     fi
     que1="${que1%\|*}"
+    Que1="${Que1%${NEWLINE}*}"
 }
 
-pth=$1
+if [[ -n $1 ]]; then
+    pth=$1
+else
+    pth='.'
+fi
 if [[ ${pth:0:1} == '-' ]]; then
     pth='.'
 else
     shift
 fi
 
-l=""
+l=1000000007
 
-while getopts ":l:rai:?" argc; do
+while getopts ":l:rasi:?" argc; do
     case $argc in
         l)
             l=$OPTARG
@@ -70,6 +130,10 @@ while getopts ":l:rai:?" argc; do
             ;;
         i)
             I=$OPTARG
+            ;;
+        s)
+            sym=1
+            c="$c -l"
             ;;
         ?)
             ;;
