71866df54f75038acefcfa9dd132849d8583a02a
Brian-Hsu <51012111+brianhsu7476@users.noreply.github.com>
2022-03-31 16:25:27 +0800

    Create vector.c

---
+++ DSA/vector.c
@@ -0,0 +1,7 @@
+#include <stdlib.h>
+int highbit(int n){int l=0, r=32; while(r-l>1){int m=l+r>>1; (1<<m)>n?(r=m):(l=m);} return 1<<l;}
+typedef struct vec{ll *a; int sz, Sz;}vec;
+void init(vec *v){v->Sz=1, v->sz=0, v->a=(ll*)malloc(v->Sz*sizeof(ll));}
+void resize(vec *v, int n){v->Sz=highbit(n)<<1, v->a=(ll*)realloc(v->a, v->Sz*sizeof(ll));}
+void push_back(vec *v, int n){if(v->sz==v->Sz)resize(v, v->sz); v->a[v->sz++]=n;}
+void pop_back(vec *v){if(v->sz<=(v->Sz>>2))resize(v, v->sz); --v->sz;}
